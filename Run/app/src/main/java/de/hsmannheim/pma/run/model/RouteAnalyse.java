0d663d280a37b01608acdf7b725c12a912533ced
aaron <Aaron2222@gmx.de>
2017-06-08 13:51:31 +0200

    Fehlende Sachen hinzugefügt

--- Run/app/src/main/java/de/hsmannheim/pma/run/model/RouteAnalyse.java
+++ Run/app/src/main/java/de/hsmannheim/pma/run/model/RouteAnalyse.java
@@ -1,5 +1,8 @@
 package de.hsmannheim.pma.run.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.android.gms.maps.model.LatLng;
 
 import java.util.Date;
@@ -12,8 +15,9 @@ import de.hsmannheim.pma.run.utils.DistanceCalculator;
  * Created by aaron on 30.05.17.
  */
 
-public class RouteAnalyse {
-    private final Route route;
+public class RouteAnalyse implements Parcelable {
+
+    private Route route;
 
     private Double distance;
     private Double paceMinPerKm;
@@ -21,15 +25,105 @@ public class RouteAnalyse {
     private Double meterUp;
     private Double meterDown;
     private long time;
+    private Integer routeId;
+    private Integer challengeId;
+    private String username;
+    private Date startDate;
+
 
 
     public RouteAnalyse(Route route){
         this.route = route;
+        routeId=-1;
+        challengeId=-1;
+        startDate = route.getStartDate();
+    }
+
+    public static final Creator<RouteAnalyse> CREATOR =
+            new Creator<RouteAnalyse>() {
+                public RouteAnalyse createFromParcel(Parcel in) {
+                    return new RouteAnalyse(in);
+                }
+
+                public RouteAnalyse[] newArray(int size) {
+                    return new RouteAnalyse[size];
+                }
+            };
+
+    private RouteAnalyse(Parcel in) {
+        distance=in.readDouble();
+        paceMinPerKm=in.readDouble();
+        speedKmh=in.readDouble();
+        meterUp=in.readDouble();
+        meterDown=in.readDouble();
+        time=in.readLong();
+        routeId=in.readInt();
+        challengeId=in.readInt();
+        username=in.readString();
+        startDate = (Date) in.readSerializable();
 
     }
 
-    public void analyseAll(){
+    public void writeToParcel(Parcel out, int flags) {
+       out.writeDouble(distance);
+        out.writeDouble(paceMinPerKm);
+        out.writeDouble(speedKmh);
+        out.writeDouble(meterUp);
+        out.writeDouble(meterDown);
+        out.writeLong(time);
+        out.writeInt(routeId);
+        out.writeInt(challengeId);
+        out.writeString(username);
+        out.writeSerializable(startDate);
+    }
+    public int describeContents() {
+        return this.hashCode();
+    }
 
+    public Double getPaceMinPerKm() {
+        return paceMinPerKm;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public Double getSpeedKmh() {
+        return speedKmh;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getRouteId() {
+        return routeId;
+    }
+
+    public Integer getChallengeId() {
+        return challengeId;
+    }
+
+    public void setRouteId(int routeId) {
+        this.routeId = routeId;
+    }
+
+    public void setChallengeId(int challangeId) {
+        this.challengeId = challangeId;
+    }
+
+    public void analyseAll(){
+        analyseDistance();
+        analyseHigh();
+        analyseTime();
     }
     public void analyseDistance(){
         distance=0d;
